5c67b7b315b54c8e5340f4b63017b07744bbc270
Volodymyr <vova2000sk@gmail.com>
2023-05-02 14:47:10 +0300

    commit

--- client/src/pages/AnnouncementPage.js
+++ client/src/pages/AnnouncementPage.js
@@ -1,11 +1,68 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from 'react';
+import { Context } from "../index";
+import { Button, Card, Col, Container, Image, Row } from "react-bootstrap";
+import { useParams } from 'react-router-dom'
+import { observer } from "mobx-react-lite";
+import heart from '../set/heart-fill.svg'
+
+
 
 const AnnouncementPage = () => {
+    const announcement = {
+        id: 2, name: "Вікторія1", price: "300",
+        img: "https://dastarkhan24.kz/upload/iblock/3a6/3a6690501bd34dead6bb520094940db2.jpg",
+        info: [{ title: "1", description: "1" }, { title: "2", description: "2" }, { title: "3", description: "3" },]
+    }
+
+    const { user } = useContext(Context);
+    const { authStore } = useContext(Context);
+
+    const { id } = useParams()
+
+    console.log(id)
+    let textBtn = "Вподобати";
+    const funcLike = () => {
+        let flag = "light";
+        authStore.likes.map(like => {
+            if (like.id === announcement.id) {
+                flag = "danger";
+                textBtn = "Додано до  "
+            }
+        })
+        return flag
+    }
+
     return (
-        <div>
-            AnnouncementPage
-        </div>
-    )
+        <Container className="mt-5">
+            <Row>
+                <Col md={4}>
+                    <Image width={300} height={300} src={announcement.img} />
+                </Col>
+                <Col md={4}>
+                    <Card
+                        className="d-flex flex-column align-items-center justify-content-around"
+                        style={{ width: 300, height: 300, fontSize: 32, border: '5px solid lightgray' }}
+                    >
+                        <h2>{announcement.name}</h2>
+                        <h3>Ціна: {announcement.price} грн.</h3>
+                        {user.isAuth ?
+                            <Button className="btnCard" variant={funcLike()}
+                            >{textBtn}  <img className="addLike" src={heart} /></Button>
+                            :
+                            <p></p>}
+                    </Card>
+                </Col>
+            </Row>
+            <Row className="d-flex flex-column m-3">
+                <h1>Додаткова інформація</h1>
+                {announcement.info.map((info, index) =>
+                    <Row key={info.id} style={{ background: index % 2 === 0 ? 'lightgray' : 'transparent', padding: 10 }}>
+                        {info.title}: {info.description}
+                    </Row>
+                )}
+            </Row>
+        </Container>
+    );
 }
 
 export default AnnouncementPage;
