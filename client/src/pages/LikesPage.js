347dfd70076f9e405e757340b7afa5896b728a9f
Volodymyr <vova2000sk@gmail.com>
2023-05-08 16:43:38 +0300

    commit LikesPage

--- client/src/pages/LikesPage.js
+++ client/src/pages/LikesPage.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { observer } from "mobx-react-lite";
+import { Card, Container, Form } from "react-bootstrap";
+import { Context } from "..";
+import OneCard from "../components/OneCard";
+import { fetchLikes } from "../http/privateAPI";
 
-const LikesPage = () => {
+
+const LikesPage = observer(() => {
+    const { user } = useContext(Context);
+    const { announcement } = useContext(Context);
+    const { authStore } = useContext(Context);
+
+    //console.log(user.user.id);
+
+    useEffect(() => {
+        fetchLikes().then((data) => authStore.setLikes(data.rows))
+    }, [])
+
+    console.log(announcement.announcements)
+
     return (
-        <div>
-            LikesPage
-        </div>
+        <Container className=" justify-content-center align-items-center" >
+            <Form className="d-flex row">
+                {announcement.announcements.map((el) => {
+                    let flag = false;
+                    authStore.likes.map((like) => {
+                        if (like.announcementId === el.id) flag = true;
+                    })
+                    if (flag) return <OneCard key={el.id} el={el} />
+                })}
+            </Form>
+
+        </Container >
     )
-}
+})
 
 export default LikesPage;
