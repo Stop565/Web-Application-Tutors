9d029770b76cf1aec17d360ad6bce6beeaf03106
Volodymyr <vova2000sk@gmail.com>
2023-05-25 16:38:02 +0300

    commit

--- client/src/components/Maps.js
+++ client/src/components/Maps.js
@@ -1,26 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { useContext } from "react";
 import { Card } from "react-bootstrap";
 import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
 import "leaflet/dist/leaflet.css";
 import "./css/maps.css"
+import { Icon } from "leaflet";
 
 
-const Map = () => {
+const Maps = () => {
+    const position = [51.505, -0.09]
+    const customIcon = new Icon({
+        iconUrl: require("../set/position.png"),
+        iconSize: [40, 40]
+    })
+
+
     return (
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[51.505, -0.09]}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
+            <Marker position={position} icon={customIcon}>
+                <Popup>Я тут</Popup>
             </Marker>
         </MapContainer>
 
     )
 }
 
-export default Map;
+export default Maps;
